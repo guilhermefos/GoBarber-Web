86b8bb8bd72c61f17dcc1330aa7c376004c40a6f
Guilherme Ferreira <guilhermefos.developer@gmail.com>
2020-08-01 17:10:24 -0300

    Add private route logic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={SignUp} />
     <Route path="/signin" component={SignIn} />
+
+    <Route path="/dashboard" component={SignIn} isPrivate />
   </Switch>
 );
 
