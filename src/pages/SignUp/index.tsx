323a06a06f95f82f298c456e9bd6b41ef0ac0fe9
Guilherme Ferreira <guilhermefos.developer@gmail.com>
2020-10-02 10:04:15 -0300

    Add missing props to UseCallback hook

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -26,36 +26,39 @@ const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const history = useHistory();
 
-  const handleSubmit = useCallback(async (data: SignUpFormData) => {
-    try {
-      formRef.current?.setErrors({});
+  const handleSubmit = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      const schema = Yup.object().shape({
-        name: Yup.string().required('Nome obrigatório'),
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('Digite um email válido'),
-        password: Yup.string().min(6, 'No minimo 6 digitos'),
-      });
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um email válido'),
+          password: Yup.string().min(6, 'No minimo 6 digitos'),
+        });
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
-      const { name, email, password } = data;
+        const { name, email, password } = data;
 
-      await api.post('users', {
-        name,
-        email,
-        password,
-      });
+        await api.post('users', {
+          name,
+          email,
+          password,
+        });
 
-      history.push('/');
-    } catch (error) {
-      const errors = getValidationErrors(error);
-      formRef.current?.setErrors(errors);
-    }
-  }, []);
+        history.push('/');
+      } catch (error) {
+        const errors = getValidationErrors(error);
+        formRef.current?.setErrors(errors);
+      }
+    },
+    [history],
+  );
 
   return (
     <Container>
